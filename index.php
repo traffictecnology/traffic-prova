08a09fbbce1ee189925fd1bdbdd1de94ae4392c9
traffictecnology <56032408+traffictecnology@users.noreply.github.com>
2019-10-01 17:57:14 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php echo "Silence is golden";
+
+
+if($update->message->text == '/salutami') {
+	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+	$response = $client->sendMessage(['chat_id' => $update->message->chat->id,
+			'text' => "Ciao, come stai?"
+	]);
+}
+
+?>
