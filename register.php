a72506d0b615444c2eff39a7c4457fa542c998d9
traffictecnology <56032408+traffictecnology@users.noreply.github.com>
2019-10-01 17:30:24 +0200

    Add files via upload

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://traffic-prova.herokuapp.com/execute.php';
+$BOT_TOKEN = '700703415:AAFWpmUeA1OcF5_mg5g1AEqjAor8Tk5B-eo';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
